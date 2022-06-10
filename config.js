f946c32f1e0bfd9da5ff0905afa7867892d0ca37
Ngô Minh Kiên <102979362+1812006@users.noreply.github.com>
2022-06-10 10:07:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
     titleWeb: "Hí, chào cậu!",
     introTitle: 'hmmm',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Cuộc đời của mình sẽ thật tuyệt nếu có cậu đó <3`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -12,7 +9,7 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'nói thế thì chịu chứ biết sao giờ',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
